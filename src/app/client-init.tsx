2f565dca54a6a604f507083ea322442cc66beeae
ildar.timerbaev <dkildar@gmail.com>
2024-09-10 13:44:50 +0300

    Fixed populating active user data on client init

--- src/app/client-init.tsx
+++ src/app/client-init.tsx
@@ -3,23 +3,27 @@ import useMount from "react-use/lib/useMount";
 import * as ls from "@/utils/local-storage";
 import Cookies from "js-cookie";
 import { initI18next } from "@/features/i18n";
-import Bugsnag from "@bugsnag/js";
+import { getAccountFullQuery } from "@/api/queries";
+import { useEffect } from "react";
 
 export function ClientInit() {
+  const activeUser = useGlobalStore((s) => s.activeUser);
   const setActiveUser = useGlobalStore((state) => state.setActiveUser);
   const updateActiveUser = useGlobalStore((state) => state.updateActiveUser);
 
+  const { data } = getAccountFullQuery(activeUser?.username).useClientQuery();
+
   useMount(() => {
     initI18next();
-
-    const activeUsername = ls.get("active_user") ?? Cookies.get("active_user");
-    if (activeUsername) {
-      setActiveUser(activeUsername);
-      updateActiveUser();
-    }
-
-    Bugsnag.setUser(activeUsername);
   });
+
+  useEffect(() => {
+    const activeUsername = ls.get("active_user") ?? Cookies.get("active_user");
+    if (activeUsername && data) {
+      setActiveUser(activeUsername);
+      updateActiveUser(data);
+    }
+  }, [data]);
 
   return <></>;
 }
--- src/features/bugsnag.tsx
+++ src/features/bugsnag.tsx
@@ -2,8 +2,11 @@ import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 import BugsnagPerformance from "@bugsnag/browser-performance";
 import React, { PropsWithChildren } from "react";
+import { useGlobalStore } from "@/core/global-store";
 
 export function BugsnagErrorBoundary(props: PropsWithChildren) {
+  const activeUser = useGlobalStore((s) => s.activeUser);
+
   if (process.env.NODE_ENV !== "production") {
     return <>{props.children}</>;
   }
@@ -14,6 +17,8 @@ export function BugsnagErrorBoundary(props: PropsWithChildren) {
   });
   BugsnagPerformance.start({ apiKey: "53c03fdd42cd699cb95f60abe77a5b19" });
   const ErrorBoundary = Bugsnag.getPlugin("react")!.createErrorBoundary(React);
+
+  Bugsnag.setUser(activeUser?.username);
 
   return <ErrorBoundary>{props.children}</ErrorBoundary>;
 }
