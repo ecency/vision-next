020e366ad394f108da73373358a2848cb2fda61f
ildar.timerbaev <dkildar@gmail.com>
2024-09-21 22:54:22 +0300

    Fixed register-device API request

--- src/api/mutations/notifications/update-notifications-settings.ts
+++ src/api/mutations/notifications/update-notifications-settings.ts
@@ -7,6 +7,10 @@ import * as ls from "@/utils/local-storage";
 import { error, success } from "@/features/shared";
 import i18next from "i18next";
 import { formatError } from "@/api/operations";
+import { getAccessToken } from "@/utils";
+import { appAxios } from "@/api/axios";
+import { apiBase } from "@/api/helper";
+import { ApiNotificationSetting } from "@/entities";
 
 export function useUpdateNotificationsSettings() {
   const activeUser = useGlobalStore((state) => state.activeUser);
@@ -14,13 +18,32 @@ export function useUpdateNotificationsSettings() {
 
   return useMutation({
     mutationKey: ["notifications", "update-settings"],
-    mutationFn: ({ notifyTypes, isEnabled }: { notifyTypes: NotifyTypes[]; isEnabled: boolean }) =>
-      saveNotificationsSettings(
-        activeUser!.username,
-        notifyTypes,
-        isEnabled,
-        ls.get("fb-notifications-token")
-      ),
+    mutationFn: async ({
+      notifyTypes,
+      isEnabled
+    }: {
+      notifyTypes: NotifyTypes[];
+      isEnabled: boolean;
+    }) => {
+      if (!activeUser?.username) {
+        throw new Error("[UpdateNotificationSettings] Attempted to update settings with anon user");
+      }
+
+      const token = ls.get("fb-notifications-token") ?? "";
+
+      const response = await appAxios.post<ApiNotificationSetting>(
+        apiBase(`/private-api/register-device`),
+        {
+          code: getAccessToken(activeUser.username),
+          username: activeUser.username,
+          token,
+          system: "web",
+          allows_notify: Number(isEnabled),
+          notify_types: notifyTypes
+        }
+      );
+      return response.data;
+    },
     onError: (e) => error(...formatError(e)),
     onSuccess: (settings) => {
       queryClient.setQueryData(
