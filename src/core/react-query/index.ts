227f776558bdcf913bfc9fe16bbbb680b80dd155
ildar.timerbaev <dkildar@gmail.com>
2024-09-28 21:46:31 +0300

    Fixed wallet SPK data fetching

--- src/core/react-query/index.ts
+++ src/core/react-query/index.ts
@@ -89,7 +89,8 @@ export enum QueryIdentifiers {
   HIVE_HBD_STATS = "hive-hbd-stats",
   GET_ORDER_BOOK = "get-order-book",
   SIMILAR_ENTRIES = "similar-entries",
-  SEARCH_BY_USERNAME = "search-by-username"
+  SEARCH_BY_USERNAME = "search-by-username",
+  SPK_USER_WALLET = "spk-user-wallet"
 }
 
 export function makeQueryClient() {
