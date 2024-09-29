3b450f0198974dea22fffb3c8719b45d1508f107
feruzm <fmiboy@gmail.com>
2024-09-29 08:41:46 +0530

    fix edithistory crash

--- src/features/shared/edit-history/hooks/use-history-list.ts
+++ src/features/shared/edit-history/hooks/use-history-list.ts
@@ -10,7 +10,7 @@ interface CommentHistoryListItemDiff {
   titleDiff?: string;
   body: string;
   bodyDiff?: string;
-  tags: string;
+  tags: string | string[];
   tagsDiff?: string;
   timestamp: string;
   v: number;
@@ -47,7 +47,7 @@ export function useHistoryList(entry: Entry) {
         title: historyData.list[l].title,
         body: h,
         timestamp: historyData.list[l].timestamp,
-        tags: historyData.list[l].tags.join(", ")
+        tags: (typeof historyData.list[l].tags === 'string' || historyData.list[l].tags instanceof String)?historyData.list[l].tags:historyData.list[l].tags.join(", ")
       });
     }
 
