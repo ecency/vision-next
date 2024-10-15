82ab130cecd212b32761489eaf722b39dab33b0e
ildar.timerbaev <dkildar@gmail.com>
2024-10-16 00:16:36 +0400

    Added loading for changing leaderboard query param

--- src/app/discover/_components/discover-period-dropdown.tsx
+++ src/app/discover/_components/discover-period-dropdown.tsx
@@ -2,13 +2,25 @@
 
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from "@ui/dropdown";
 import i18next from "i18next";
-import React from "react";
-import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { chevronDownSvgForSlider } from "@ui/svg";
 import { Button } from "@ui/button";
+import { UilSpinner } from "@tooni/iconscout-unicons-react";
 
 export function DiscoverPeriodDropdown() {
   const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const params = useSearchParams();
+
+  useEffect(() => {
+    if (params.get("period")) {
+      setIsLoading(false);
+    }
+  }, [params]);
+
   return (
     <Dropdown>
       <DropdownToggle>
@@ -18,12 +30,19 @@ export function DiscoverPeriodDropdown() {
           size="sm"
           appearance="gray-link"
         >
+          {isLoading && <UilSpinner className="w-4 h-4 mr-4 text-blue-dark-sky animate-spin" />}
           {i18next.t("leaderboard.title-stars")}
         </Button>
       </DropdownToggle>
       <DropdownMenu align="right">
         {["day", "week", "month"].map((f) => (
-          <DropdownItem key={f} onClick={() => router.push(`/discover?period=${f}`)}>
+          <DropdownItem
+            key={f}
+            onClick={() => {
+              setIsLoading(true);
+              router.push(`/discover?period=${f}`);
+            }}
+          >
             {i18next.t(`leaderboard.period-${f}`)}
           </DropdownItem>
         ))}
