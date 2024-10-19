055f66c72c42391907246520cfcf0aec3f0ea5f7
ildar.timerbaev <dkildar@gmail.com>
2024-10-19 22:57:12 +0300

    Added try catch for all localstorage interactions

--- src/features/banners/index.tsx
+++ src/features/banners/index.tsx
@@ -4,7 +4,10 @@ import { NoLocalStorageBanner } from "@/features/banners/no-local-storage-banner
 import { useMemo } from "react";
 
 export function BannerManager() {
-  const hasLocalStorage = useMemo(() => "localStorage" in window, []);
+  const hasLocalStorage = useMemo(
+    () => (typeof window !== "undefined" ? "localStorage" in window : true),
+    []
+  );
 
   return (
     <div className="fixed bottom-0 left-0 right-0 z-[9999]">
