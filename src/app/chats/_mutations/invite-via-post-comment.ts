880a553c3b41ea74a4fc4f6dd30ccb7092b8c054
ildar.timerbaev <dkildar@gmail.com>
2024-10-23 23:34:07 +0300

    https://github.com/ecency/vision-next/issues/113
    Fixed chats invitation

--- src/app/chats/_mutations/invite-via-post-comment.ts
+++ src/app/chats/_mutations/invite-via-post-comment.ts
@@ -2,41 +2,34 @@ import { useMutation } from "@tanstack/react-query";
 import { useGlobalStore } from "@/core/global-store";
 import { getAccountPostsQuery } from "@/api/queries";
 import { useCreateReply } from "@/api/mutations";
-import { createReplyPermlink, tempEntry } from "@/utils";
-import { comment } from "@/api/operations";
-import { FullAccount } from "@/entities";
+import { createReplyPermlink } from "@/utils";
 import { error } from "@/features/shared";
 import i18next from "i18next";
 import { useMemo } from "react";
+import useMount from "react-use/lib/useMount";
 
 export function useInviteViaPostComment(username: string) {
   const activeUser = useGlobalStore((state) => state.activeUser);
-  const { data } = getAccountPostsQuery(username).useClientQuery();
+  const { data, refetch } = getAccountPostsQuery(username).useClientQuery();
 
   const posts = useMemo(() => data?.pages[0] ?? [], [data]);
 
   const { mutateAsync: addReply } = useCreateReply(posts[0]);
 
+  useMount(() => refetch());
+
   return useMutation({
     mutationKey: ["chats/invite-via-post-comment"],
     mutationFn: async (text: string) => {
       if (posts[0]) {
-        const { author: parentAuthor, permlink: parentPermlink } = posts[0];
         const author = activeUser!!.username;
         const permlink = createReplyPermlink(author);
-        await comment(author, parentAuthor, parentPermlink, permlink, "", text, {}, null, true);
-        return addReply(
-          tempEntry({
-            author: activeUser!!.data as FullAccount,
-            permlink,
-            parentAuthor,
-            parentPermlink,
-            title: "",
-            body: text,
-            tags: [],
-            description: null
-          }) as any
-        );
+        return addReply({
+          text,
+          permlink,
+          jsonMeta: {},
+          point: false
+        });
       } else {
         error(i18next.t("chat.no-posts-for-invite"));
         throw new Error(i18next.t("chat.no-posts-for-invite"));
