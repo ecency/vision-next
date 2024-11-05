b1485cc57392c985dba8d9538dd71fecffffde93
ildar.timerbaev <dkildar@gmail.com>
2024-11-05 23:03:10 +0300

    Fixed dark styles of checkboxes

--- src/features/ui/input/form-controls/toggle.tsx
+++ src/features/ui/input/form-controls/toggle.tsx
@@ -19,15 +19,16 @@ export function Toggle({ checked, onChange, label, disabled }: ToggleProps) {
     >
       <div
         className={classNameObject({
-          "border-2 rounded-full bg-gray-100 p-0.5 w-[2.5rem] duration-300": true,
-          "bg-green border-green": checked
+          "rounded-full p-0.5 w-[2.5rem] duration-300": true,
+          "border-2 bg-green border-green": checked,
+          "border-2 dark:border-gray-900 bg-gray-100 dark:bg-gray-800": !checked
         })}
       >
         <div
           className={classNameObject({
             "ecency-toggle-icon rounded-full w-3.5 h-3.5 duration-300": true,
             "bg-white translate-x-[1.125rem]": checked,
-            "bg-gray-300": !checked,
+            "bg-gray-300 dark:bg-gray-600": !checked,
             "opacity-50": disabled
           })}
         />
