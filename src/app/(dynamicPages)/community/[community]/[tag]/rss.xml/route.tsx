3311e347ffbb333a3fade01d1fd659f502848b4c
ildar.timerbaev <dkildar@gmail.com>
2024-11-25 23:53:20 +0300

    Fixed community RSS xml

--- src/app/(dynamicPages)/community/[community]/[tag]/rss.xml/route.tsx
+++ src/app/(dynamicPages)/community/[community]/[tag]/rss.xml/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { AccountRssHandler } from "@/features/rss";
+import { CommunityRssHandler } from "@/features/rss";
 
 interface Props {
   params: Promise<{ community: string; tag: string }>;
@@ -8,7 +8,7 @@ interface Props {
 export async function GET(request: NextRequest, { params }: Props) {
   const { community, tag } = await params;
   return new Response(
-    (await new AccountRssHandler(request.nextUrl.pathname, community, tag).getFeed()).xml(),
+    (await new CommunityRssHandler(request.nextUrl.pathname, community, tag).getFeed()).xml(),
     { headers: { "Content-Type": "text/xml" } }
   );
 }
