6ecb9ba3f71fc947815139c6ff2768b8559f1ed0
ildar.timerbaev <dkildar@gmail.com>
2024-12-02 09:43:05 +0300

    Added loading state for waves page

---
+++ src/app/waves/loading.tsx
@@ -0,0 +1,17 @@
+import { WaveFormLoading } from "@/features/waves";
+import { WavesNavigationLayout } from "@/app/waves/_components";
+
+export default function WavesDetailsLoading() {
+  return (
+    <div className="flex flex-col gap-4 lg:gap-6 xl:gap-8">
+      <div className="rounded-2xl bg-white dark:bg-dark-200 mb-4 lg:mb-6 xl:mb-8">
+        <WavesNavigationLayout>
+          <div className="p-4">
+            <div className="animate-pulse h-[20px] rounded-lg w-[20px] bg-blue-dark-sky-040 dark:bg-blue-dark-grey" />
+          </div>
+        </WavesNavigationLayout>
+        <WaveFormLoading isReply={false} />
+      </div>
+    </div>
+  );
+}
