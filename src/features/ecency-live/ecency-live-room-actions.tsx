c30cc0649f7e4f94cd0295f4b50e69e5dcbb7743
ildar.timerbaev <dkildar@gmail.com>
2024-12-16 09:17:27 +0300

    Added mic mute/unmute in live

--- src/features/ecency-live/ecency-live-room-actions.tsx
+++ src/features/ecency-live/ecency-live-room-actions.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@ui/button";
-import { UilEmoji, UilExit } from "@tooni/iconscout-unicons-react";
+import {
+  UilEmoji,
+  UilExit,
+  UilMicrophone,
+  UilMicrophoneSlash
+} from "@tooni/iconscout-unicons-react";
 import i18next from "i18next";
 import { useEcencyLive } from "@/features/ecency-live/ecency-live-context";
-import { motion } from "framer-motion";
-import { useCallback, useMemo } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useCallback, useMemo, useState } from "react";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from "@ui/dropdown";
+import { useMount } from "react-use";
+import { StateType } from "jam-core";
+
+type PeerState = StateType["peerState"][1];
 
 interface Props {
   onLeave: () => void;
@@ -13,9 +22,29 @@ interface Props {
 export function EcencyLiveRoomActions({ onLeave }: Props) {
   const live = useEcencyLive();
 
+  const [currentUser, setCurrentUser] = useState<string>();
+  const [room, setRoom] = useState<StateType["room"]>();
+  const [state, setState] = useState<PeerState>();
   const prebuiltReactionsList = useMemo(() => ["🔥", "💯", "😀", "🤣", "🤔", "🤘"], []);
 
   const sendReaction = useCallback((reaction: string) => live?.[1].sendReaction(reaction), [live]);
+  const toggleMic = useCallback(() => {
+    if (live?.[0].myAudio?.active) {
+      live?.[1].setProps("micMuted", !state?.micMuted);
+    } else {
+      live?.[1].retryMic();
+    }
+  }, [live, state?.micMuted]);
+
+  useMount(() => {
+    setCurrentUser(live?.[0].myId ?? undefined);
+    setRoom(live?.[0].room);
+    setState(live?.[0].myPeerState as PeerState);
+
+    live?.[1].onState("myPeerState", (state) => setState(state as PeerState));
+    live?.[1].onState("room", (value) => setRoom(value as StateType["room"]));
+    live?.[1].onState("myId", (value) => setCurrentUser(value as string));
+  });
 
   return (
     <motion.div
@@ -25,6 +54,25 @@ export function EcencyLiveRoomActions({ onLeave }: Props) {
       key="actions"
       className="flex items-center gap-2 p-4 justify-center absolute bottom-0 left-0 w-full"
     >
+      <AnimatePresence>
+        {room?.speakers.includes(currentUser ?? "") && (
+          <motion.div
+            key="mic"
+            initial={{ opacity: 0, scale: 0.875 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.875 }}
+          >
+            <Button
+              icon={state?.micMuted ? <UilMicrophoneSlash /> : <UilMicrophone />}
+              size="sm"
+              className="w-[34px]"
+              noPadding={true}
+              appearance="secondary"
+              onClick={toggleMic}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Dropdown>
         <DropdownToggle>
           <Button
