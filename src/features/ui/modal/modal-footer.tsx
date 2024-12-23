8b04b43068d67c7a9b2d2bf27a87cb5d446cb594
ildar.timerbaev <dkildar@gmail.com>
2024-12-23 09:58:24 +0300

    Fixed wave creation with a poll

--- src/features/ui/modal/modal-footer.tsx
+++ src/features/ui/modal/modal-footer.tsx
@@ -1,10 +1,12 @@
 import React, { HTMLProps } from "react";
-import { classNameObject } from "@ui/util";
+import { classNameObject, useFilteredProps } from "@ui/util";
 
 export function ModalFooter(props: HTMLProps<HTMLDivElement> & { sticky?: boolean }) {
+  const filteredProps = useFilteredProps(props, ["sticky"]);
+
   return (
     <div
-      {...props}
+      {...filteredProps}
       className={classNameObject({
         "p-3": true,
         "sticky bottom-0 bg-white border-t border-[--border-color]": props.sticky ?? false,
