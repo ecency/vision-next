776763f9b214ca18392b7ac56f7a480a9f2e2bb1
ildar.timerbaev <dkildar@gmail.com>
2025-03-11 23:41:56 +0300

    Improved signup page styles and texts

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -44,10 +44,11 @@ export default function Page() {
       <div className="container px-2 mx-auto mt-6 md:mt-8">
         <div className="grid grid-cols-12 mb-10 items-center gap-4 md:gap-6 lg:gap-8 xl:gap-10">
           <div className="col-span-12 md:col-span-6">
-            <h1 className="text-xl md:text-3xl font-bold md:leading-[3rem]">
+            <h1 className="text-blue-dark-sky text-xl md:text-3xl font-bold md:leading-[3rem]">
               {i18next.t("sign-up.header")}
             </h1>
             <h2 className="text-lg md:text-2xl">{i18next.t("sign-up.description")}</h2>
+            <p className="mt-4">{i18next.t("sign-up.description-2")}</p>
           </div>
 
           <div className="col-span-12 md:col-span-6 top-16 rounded-2xl overflow-hidden">
@@ -56,7 +57,7 @@ export default function Page() {
               height={1920}
               src="/assets/signup-main.svg"
               alt=""
-              className="w-full max-h-[400px] object-cover"
+              className="w-full max-h-[400px] object-fit"
             />
           </div>
         </div>
