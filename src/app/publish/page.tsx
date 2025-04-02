39c59df70041b9a1d2188323cc0f26efc5e57494
Ildar Timerbaev <itimerbaev@icloud.com>
2025-04-02 11:41:10 +0300

    Added back to submit page alert in publish page

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -13,11 +13,17 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 import { PublishSuccessState } from "./_components/publish-success-state";
 import { PublishEditorPollEditor } from "./_editor-extensions";
+import Link from "next/link";
+import i18next from "i18next";
+import { Alert, Button } from "@/features/ui";
+import { useLocalStorage } from "react-use";
+import { PREFIX } from "@/utils/local-storage";
 
 export default function PublishPage() {
   const editor = usePublishEditor();
 
   const [step, setStep] = useState<"edit" | "validation" | "scheduled" | "published">("edit");
+  const [showBackToSubmit, setShowBackToSubmit] = useLocalStorage(PREFIX + "_pub_back_old", true);
 
   return (
     <AnimatePresence>
@@ -38,6 +44,26 @@ export default function PublishPage() {
 
           <div className="publish-page max-w-[800px] rounded-2xl bg-white container mx-auto mt-4">
             <PublishEditorPollEditor />
+
+            {showBackToSubmit && (
+              <div className="p-4">
+                <Alert className="w-full justify-between flex items-center">
+                  {i18next.t("publish.back-to-old")}
+                  <div className="flex gap-4">
+                    <Link href="/submit">
+                      <Button size="sm">{i18next.t("g.back")}</Button>
+                    </Link>
+                    <Button
+                      onClick={() => setShowBackToSubmit(false)}
+                      size="sm"
+                      appearance="gray-link"
+                    >
+                      {i18next.t("g.close")}
+                    </Button>
+                  </div>
+                </Alert>
+              </div>
+            )}
           </div>
 
           <PublishActionBar onPublish={() => setStep("validation")} />
