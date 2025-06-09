be0b82d47d498f11e70448473203c746bb5014f0
ildar.timerbaev <dkildar@gmail.com>
2025-06-09 23:26:36 +0300

    Get rid of popper js, ECENCY-NEXT-D4Q fixed

--- src/app/(dynamicPages)/entry/[category]/[author]/[permlink]/_components/selection-popover/index.tsx
+++ src/app/(dynamicPages)/entry/[category]/[author]/[permlink]/_components/selection-popover/index.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import React, { useContext, useState } from "react";
-import { Manager, Popper } from "react-popper";
+import { useCallback, useContext, useState } from "react";
 
-import SelectionReference from "./selection-reference";
-import "./index.scss";
-import { ClickAwayListener } from "@/features/shared";
-import { EntryPageContext } from "../context";
+import { Button } from "@/features/ui";
+import { flip, shift } from "@floating-ui/dom";
+import { autoUpdate, useFloating } from "@floating-ui/react-dom";
+import { UilClipboardAlt, UilComment, UilTwitter } from "@tooni/iconscout-unicons-react";
+import Link from "next/link";
+import { createPortal } from "react-dom";
 import { useCopyToClipboard, useMountedState } from "react-use";
-import { copyContent, quotes, twitterSvg } from "@ui/svg";
+import { EntryPageContext } from "../context";
 
-let tooltipStyle = {
-  background: "rgb(0 0 0 / 78%)",
-  maxWidth: "50%",
-  borderRadius: 6
-};
+// https://github.com/FezVrasta/react-popper#usage-without-a-reference-htmlelement
+class VirtualSelectionReference {
+  selection: Selection;
+  constructor(selection: Selection) {
+    this.selection = selection;
+  }
+
+  get clientWidth() {
+    return this.getBoundingClientRect()?.width ?? 0;
+  }
+
+  get clientHeight() {
+    return this.getBoundingClientRect()?.height ?? 0;
+  }
+
+  getBoundingClientRect() {
+    if (this.selection.rangeCount > 0) {
+      return this.selection.getRangeAt(0).getBoundingClientRect();
+    }
+  }
+}
 
 export const SelectionPopover = ({ children, postUrl }: any) => {
   const { setSelection, commentsInputRef } = useContext(EntryPageContext);
@@ -22,78 +39,71 @@ export const SelectionPopover = ({ children, postUrl }: any) => {
   const [_, copyToClipboard] = useCopyToClipboard();
   const isMounted = useMountedState();
 
-  let [selectedText, setSelectedText] = useState("");
+  const [selectedText, setSelectedText] = useState("");
+
+  const { refs, floatingStyles } = useFloating({
+    whileElementsMounted: autoUpdate,
+    middleware: [flip(), shift()],
+    placement: "top",
+    transform: true
+  });
 
   const onQuotesClick = (text: string) => {
     setSelection(`>${text}\n\n`);
     commentsInputRef?.current?.focus();
   };
 
-  return (
-    <div>
-      <Manager>
-        <SelectionReference onSelect={(selection: any) => setSelectedText(selection.toString())}>
-          {(
-            getProps: (arg0: {
-              onMouseUp: () => void;
-            }) => JSX.IntrinsicAttributes &
-              React.ClassAttributes<HTMLParagraphElement> &
-              React.HTMLAttributes<HTMLParagraphElement>
-          ) => (
-            <div
-              {...getProps({
-                onMouseUp: () => {}
-              })}
-            >
-              {children}
-            </div>
-          )}
-        </SelectionReference>
+  const handleSelection = useCallback(() => {
+    const selection = window.getSelection();
+    if (selection && !selection.isCollapsed) {
+      refs.setReference(new VirtualSelectionReference(selection) as any);
+    }
+  }, [refs]);
 
-        {isMounted() && (
-          <Popper placement="top">
-            {({ ref, style, placement, arrowProps }) =>
-              selectedText.length !== 0 && (
-                <ClickAwayListener
-                  onClickAway={() => {
-                    setSelectedText("");
-                  }}
-                >
-                  <div
-                    ref={ref}
-                    style={{ ...style, ...tooltipStyle }}
-                    className="p-2 flex icons-container items-center"
-                  >
-                    <div onClick={() => copyToClipboard(selectedText)} className="pointer">
-                      {copyContent}
-                    </div>
-                    <a
-                      onClick={() => setSelectedText("")}
-                      href={`https://twitter.com/intent/tweet?text=${selectedText} https://ecency.com${postUrl}`}
-                      target="_blank"
-                      className="mx-2 pointer twitter"
-                    >
-                      {twitterSvg}
-                    </a>
-                    <div
-                      onClick={() => {
-                        setSelectedText("");
-                        onQuotesClick(selectedText);
-                        document
-                          .getElementsByClassName("comment-box")[0]
-                          .scrollIntoView({ block: "center" });
-                      }}
-                      className="pointer quotes"
-                    >
-                      {quotes}
-                    </div>
-                  </div>
-                </ClickAwayListener>
-              )
-            }
-          </Popper>
+  return (
+    <div onMouseUp={handleSelection}>
+      {children}
+
+      {isMounted() &&
+        createPortal(
+          <div
+            ref={refs.setFloating}
+            style={floatingStyles}
+            className="p-2 flex border border-[--border-color] rounded-xl gap-3 bg-white items-center"
+          >
+            <Button
+              noPadding={true}
+              icon={<UilClipboardAlt />}
+              appearance="gray-link"
+              onClick={() => copyToClipboard(selectedText)}
+            />
+            <Link
+              href={`https://twitter.com/intent/tweet?text=${selectedText} https://ecency.com${postUrl}`}
+              target="_external"
+            >
+              <Button
+                noPadding={true}
+                icon={<UilTwitter />}
+                appearance="gray-link"
+                onClick={() => setSelectedText("")}
+              />
+            </Link>
+
+            <Button
+              noPadding={true}
+              icon={<UilComment />}
+              appearance="gray-link"
+              onClick={() => {
+                setSelectedText("");
+                onQuotesClick(selectedText);
+                document
+                  .getElementsByClassName("comment-box")[0]
+                  .scrollIntoView({ block: "center" });
+              }}
+            />
+          </div>,
+          document.querySelector("#popper-container") as HTMLElement
         )}
-      </Manager>
     </div>
   );
 };
