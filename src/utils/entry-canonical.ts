c78095e66a2bb2b8acc72486192599cbba7c0b4a
feruz <fmiboy@gmail.com>
2025-06-25 19:49:42 +0300

    fix entry apps info

--- src/utils/entry-canonical.ts
+++ src/utils/entry-canonical.ts
@@ -3,33 +3,31 @@ import apps from "@hiveio/hivescript/apps.json";
 import defaults from "@/defaults.json";
 import { appName } from "./app-name";
 
-export function entryCanonical(entry: Entry, isAmp: boolean = false): string | null {
+export function entryCanonical(entry: Entry, isAmp = false): string | null {
   if (isAmp) {
     return `${defaults.base}${entry.url}`;
   }
 
-  if (entry.json_metadata?.canonical_url) {
-    return entry.json_metadata?.canonical_url.replace("https://www.", "https://");
+  const canonicalFromMetadata = entry.json_metadata?.canonical_url;
+  if (canonicalFromMetadata) {
+    return canonicalFromMetadata.replace("https://www.", "https://");
   }
 
-  let scheme = `${defaults.base}/{category}/@{username}/{permlink}`;
+  const app = appName(entry.json_metadata?.app);
+  const identifier = app?.split("/")[0];
 
-  const app = appName(entry.json_metadata.app);
-
-  if (app) {
-    const identifier = app.split("/")[0] as keyof typeof apps;
-
-    if (apps[identifier]) {
-      scheme = (apps[identifier] as { url_scheme: string }).url_scheme;
-    }
-  }
-
-  if (!scheme) {
+  if (!identifier || ["ecency", "esteem"].includes(identifier)) {
     return null;
   }
 
-  return scheme
-    .replace("{category}", entry.category)
-    .replace("{username}", entry.author)
-    .replace("{permlink}", entry.permlink);
+  const appInfo = apps[identifier] as { url_scheme?: string };
+  if (!appInfo?.url_scheme) {
+    return null;
+  }
+
+  return appInfo.url_scheme
+      .replace("{category}", entry.category)
+      .replace("{username}", entry.author)
+      .replace("{permlink}", entry.permlink);
 }
+
