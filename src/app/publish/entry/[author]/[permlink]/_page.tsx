ab7d1f16e0114138c79060ec4911c0ac80f417a6
Ildar Timerbaev <itimerbaev@icloud.com>
2025-06-28 23:33:18 +0300

    Allowed edit post's location in a publish page

--- src/app/publish/entry/[author]/[permlink]/_page.tsx
+++ src/app/publish/entry/[author]/[permlink]/_page.tsx
@@ -30,7 +30,7 @@ export default function Publish() {
 
   const { editor, setEditorContent } = usePublishEditor(() => setShowHtmlWarning(true));
 
-  const { setTitle, setContent, setTags, setMetaDescription, setSelectedThumbnail } =
+  const { setTitle, setContent, setTags, setMetaDescription, setSelectedThumbnail, setLocation } =
     usePublishState();
 
   useEntryDetector(
@@ -47,6 +47,7 @@ export default function Publish() {
         setContent(entry.body); // todo
         setMetaDescription(entry.json_metadata?.description ?? postBodySummary(entry.body, 200));
         entry?.json_metadata?.image && setSelectedThumbnail(entry?.json_metadata?.image[0]);
+        entry?.json_metadata?.location && setLocation(entry?.json_metadata?.location);
 
         setEditorContent(entry.body);
       } else {
