5cf3ed920c90d847ef9519d0929f0277f24abea4
ildar.timerbaev <dkildar@gmail.com>
2025-07-28 22:59:32 +0300

    Added community beneficiary to publish page

--- src/app/publish/_components/publish-action-bar-community.tsx
+++ src/app/publish/_components/publish-action-bar-community.tsx
@@ -2,38 +2,51 @@ import { CommunitySelector } from "@/app/submit/_components";
 import { isCommunity } from "@/utils";
 import i18next from "i18next";
 import { usePublishState } from "../_hooks";
+import { useMemo } from "react";
 
 export function PublishActionBarCommunity() {
-  const { tags, setTags } = usePublishState();
+  const { tags, setTags, beneficiaries } = usePublishState();
+
+  const beneficiaryReward = useMemo(
+    () =>
+      isCommunity(tags?.[0])
+        ? beneficiaries?.find((ben) => ben.account === tags?.[0])?.weight
+        : undefined,
+    [beneficiaries, tags]
+  );
 
   return (
-    <div className="flex flex-wrap items-center gap-1">
-      {isCommunity(tags?.[0]) && (
-        <span className="text-sm opacity-50 hidden sm:block">
-          {i18next.t("decks.threads-form.thread-host")}
-        </span>
-      )}
-      <CommunitySelector
-        tags={tags ?? []}
-        onSelect={(prev, next) => {
-          if (next === null) {
-            // Selecting "My blog"
-            const updated = [...(tags ?? [])].filter((tag) => !tag.startsWith("hive-"));
-            setTags(updated);
-            return;
-          }
+    <div className="bg-white rounded-xl p-2 flex flex-wrap gap-1">
+      <div className="text-sm opacity-50 pt-1.5">{i18next.t("decks.threads-form.thread-host")}</div>
 
-          const current = tags?.filter((tag) => tag !== next) ?? [];
+      <div>
+        <CommunitySelector
+          tags={tags ?? []}
+          onSelect={(prev, next) => {
+            if (next === null) {
+              // Selecting "My blog"
+              const updated = [...(tags ?? [])].filter((tag) => !tag.startsWith("hive-"));
+              setTags(updated);
+              return;
+            }
 
-          if (next.startsWith("hive-")) {
-            // Remove any existing hive- community tag
-            const withoutHive = current.filter((tag) => !tag.startsWith("hive-"));
-            setTags([next, ...withoutHive]);
-          } else {
-            setTags([...current, next]);
-          }
-        }}
-      />
+            const current = tags?.filter((tag) => tag !== next) ?? [];
+
+            if (next.startsWith("hive-")) {
+              // Remove any existing hive- community tag
+              const withoutHive = current.filter((tag) => !tag.startsWith("hive-"));
+              setTags([next, ...withoutHive]);
+            } else {
+              setTags([...current, next]);
+            }
+          }}
+        />
+        {beneficiaryReward && (
+          <div className="text-xs text-end text-blue-dark-sky">
+            Community reward: {beneficiaryReward / 100}%
+          </div>
+        )}
+      </div>
     </div>
   );
 }
