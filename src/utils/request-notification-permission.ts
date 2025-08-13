f80dc6c4c88c416d0d2c12b3da047aa25e9101b8
feruzm <fmiboy@gmail.com>
2025-08-13 14:09:35 +0300

    fix ECENCY-NEXT-PKZ and ECENCY-NEXT-PHA

--- src/utils/request-notification-permission.ts
+++ src/utils/request-notification-permission.ts
@@ -1,12 +1,19 @@
 export function requestNotificationPermission(): Promise<NotificationPermission> {
   return new Promise((resolve) => {
+    if (typeof window === "undefined" || typeof Notification === "undefined") {
+      resolve("denied");
+      return;
+    }
+
     const request = Notification.requestPermission();
     // In Safari, it could return undefined
     if (request === undefined) {
       Notification.requestPermission((permission) => {
         resolve(permission);
       });
+      return;
     }
+
     request.then((permission) => resolve(permission));
   });
 }
