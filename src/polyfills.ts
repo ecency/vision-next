f4cefa477a2a6e76f7eecbc6963304d1750206d4
feruz <fmiboy@gmail.com>
2025-08-27 19:59:19 +0300

    polyfill intl

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,3 +14,24 @@ if (!Array.prototype.indexOf) {
     configurable: true,
   });
 }
+
+// Polyfill Intl for environments that lack it (e.g., Node with minimal ICU)
+if (typeof Intl === "undefined" || !(Intl as any).DateTimeFormat) {
+  const IntlPolyfill = require("intl");
+  (global as any).Intl = IntlPolyfill;
+
+  // Load locale data required by the application
+  require("intl/locale-data/jsonp/en.js");
+  require("intl/locale-data/jsonp/bg.js");
+  require("intl/locale-data/jsonp/es.js");
+  require("intl/locale-data/jsonp/fi.js");
+  require("intl/locale-data/jsonp/hi.js");
+  require("intl/locale-data/jsonp/id.js");
+  require("intl/locale-data/jsonp/it.js");
+  require("intl/locale-data/jsonp/pt.js");
+  require("intl/locale-data/jsonp/ru.js");
+  require("intl/locale-data/jsonp/sr.js");
+  require("intl/locale-data/jsonp/uk.js");
+  require("intl/locale-data/jsonp/uz.js");
+  require("intl/locale-data/jsonp/zh.js");
+}
