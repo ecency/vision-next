513c0ca0dec7ee386ffda9e3f7663cac5902f17d
feruz <fmiboy@gmail.com>
2025-10-15 07:25:32 +0300

    sort hosts

--- apps/web/src/features/waves/consts/available-hosts.ts
+++ apps/web/src/features/waves/consts/available-hosts.ts
@@ -3,7 +3,7 @@ import { WaveHosts } from "@/features/waves/enums";
 export const AVAILABLE_THREAD_HOSTS = [
   ...(process.env.NODE_ENV === "development" ? [WaveHosts.Testhreads] : []),
   WaveHosts.Waves,
+  WaveHosts.PeakSnaps,
   WaveHosts.Liketu,
-  WaveHosts.Leo,
-  WaveHosts.PeakSnaps
+  WaveHosts.Leo
 ];
