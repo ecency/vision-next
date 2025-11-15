0d1439b01ac31f090b236477e85d8889f5c7f876
feruzm <fmiboy@gmail.com>
2025-11-15 17:43:25 +0530

    fix import

--- apps/web/src/app/proposals/_components/index.ts
+++ apps/web/src/app/proposals/_components/index.ts
@@ -2,8 +2,3 @@ export * from "./proposal-list-item";
 export * from "./proposal-votes";
 export * from "./proposal-vote-btn";
 export * from "./proposal-create-form";
-export * from "./proposal-create-form";
-export * from "./proposal-create-form";
-export * from "./proposal-create-form";
-export * from "./proposal-create-form";
-export * from "./proposal-create-form";
