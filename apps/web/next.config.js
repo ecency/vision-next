bf03736a0ffe144de446ac1e63e99f6384f9dcae
feruz <fmiboy@gmail.com>
2025-12-04 20:10:09 +0200

    Revert "eslint fix"
    This reverts commit 6985d400168719576e0f7b7442a8c234653f8a2a.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,17 +8,19 @@ const withPWA = require("next-pwa")({
 });
 const appPackage = require("./package.json");
 const { v4 } = require("uuid");
+const sassEmbedded = require("sass-embedded");
 
 const config = {
   productionBrowserSourceMaps: true,
   htmlLimitedBots:
     /Mediapartners-Google|Chrome-Lighthouse|Slurp|DuckDuckBot|baiduspider|yandex|sogou|bitlybot|tumblr|vkShare|quora link preview|redditbot|ia_archiver|Bingbot|BingPreview|applebot|facebookexternalhit|facebookcatalog|Twitterbot|LinkedInBot|Slackbot|Discordbot|WhatsApp|SkypeUriPreview|Yeti/,
   sassOptions: {
-    implementation: require.resolve("sass-embedded"),
+    implementation: sassEmbedded,
     includePaths: [path.join(__dirname), path.join(__dirname, "src/styles")],
     silenceDeprecations: ["legacy-js-api", "import", "global-builtin", "color-functions"]
   },
   generateBuildId: async () => v4(),
+  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   transpilePackages: ["@ecency/sdk", "@ecency/wallets", "@ecency/renderer"],
   experimental: {
